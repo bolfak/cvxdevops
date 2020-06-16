e91e3924a41f46eea75fb2ca678be6951cc9deed
Mobolaji Fakeyede <mobolaji.fakeyede@gmail.com>
2020-06-16 14:54:59 +0100

    Local Build

---
+++ Services/CalculatorService.cs
@@ -0,0 +1,18 @@
+using System;
+public class CalculatorService{
+    public double Add(double number1, double number2){
+        return number1 + number2;
+    }
+
+    public double Subtract(double number1, double number2){
+        return number1 - number2;
+    }
+
+    public double Multiply(double number1, double number2){
+        return number1 * number2;
+    }
+
+    public double Divide(double number1, double number2){
+        return number1 / number2;
+    }
+}
